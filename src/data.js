bcc06edf66a9675bcfdc49a1e86787331c0fb56a
arafin0 <sultanularafin000@gmail.com>
2022-03-26 20:10:38 +0600

    fake data added

---
+++ src/data.js
@@ -0,0 +1,62 @@
+const data = [
+    {
+      id:"1",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Oranges_-_whole-halved-segment.jpg/1200px-Oranges_-_whole-halved-segment.jpg",
+      text: "Orange",
+      price: 5
+    },
+    {
+      id:"2",
+      image: "https://wallpaperaccess.com/full/1351657.jpg",
+      text: "Apple",
+      price: 15
+    },
+    {
+      id:"3",
+      image: "https://cdn1.sph.harvard.edu/wp-content/uploads/sites/30/2018/08/bananas-1354785_1920-1200x800.jpg",
+      text: "Banana",
+      price: 5
+    },
+    {
+      id:"4",
+      image: "https://www.hindimeaning.com/pictures/fruits/Apricot.jpg",
+      text: "Apricot",
+      price: 50
+    },
+    {
+      id:"5",
+      image: "https://www.hindimeaning.com/pictures/fruits/Black%20Currant.jpg",
+      text: "Black Currant",
+      price: 20
+    },
+    {
+      id:"6",
+      image: "https://www.hindimeaning.com/pictures/fruits/coconuts.jpeg",
+      text: "Coconuts",
+      price: 10
+    },
+    {
+      id:"7",
+      image: "https://www.hindimeaning.com/pictures/fruits/Custard%20Apple.jpg",
+      text: "Custard Apple",
+      price: 4
+    }, {
+      id:"8",
+      image: "https://www.hindimeaning.com/pictures/fruits/Grapes.jpg",
+      text: "Grapes",
+      price: 11
+    }, {
+      id:"9",
+      image: "https://www.hindimeaning.com/pictures/fruits/Mango.jpg",
+      text: "Mango",
+      price: 2
+    }, {
+      id:"10",
+      image: "https://www.hindimeaning.com/pictures/fruits/Peach.png",
+      text: "Peach",
+      price: 20
+    }
+  ];
+  
+  export default data;
+  
